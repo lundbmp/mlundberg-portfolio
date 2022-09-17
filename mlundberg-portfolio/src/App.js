a540c8c4fbc9c1a08b611e202988140862a18095
lundbmp <m.lundberg008@gmail.com>
2022-09-17 11:34:19 -0500

    set up files, going into dev

---
+++ mlundberg-portfolio/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import About from "./components/About";
+import Projects from "./components/Projects";
+
+function App() {
+  return (
+    <div>
+      <About></About>
+    </div>
+  );
+}
+
+export default App;
---
+++ mlundberg-portfolio/src/components/About/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function About() {
+    return (
+        <article>
+            <h2>About Me</h2>
+            <p>
+                Thanks for finding my page.
+            </p>
+            <p>
+                My name is Michael, I'm a graduate of North Hennepin Community College with an assiociate in Computer Science.
+                At North Hennepin I learned about: Object Oriented Programming, Data Structures, Algorithms, Computer Architecture(ARM specific), and Databases.
+                While at North Hennepin I became proficient in Java, C#, Python, C++, and SQL.
+            </p>
+            <p>
+                I'm currently going the University of Minnesota Coding Bootcamp. We're leanining the MongoDB, Express.js, React, Node.js stack(MERN).
+                Throughout this bootcamp, I've learned about the importance of version control, setting up API routes, and working in a group.
+                The projects Boozy Books and Suds on Sale were both completed as group projects.
+            </p>
+            <p>
+                I currently work at ECMC as an Operations Analyst. At ECMC I work with developers to report issues and enhancments,
+                gather and compile information for research, automation of simple tasks, and making sure our systems adhere to 
+                federal guidelines. While at ECMC I've learned how to work in an Agile enviroment, managing and creating 
+                procedural documentation, as well as making complex SQL queries. 
+            </p>
+            <p>
+                Work life balance is important and how I manage that is by having a variety of hobbies. Outside of work I like to: 
+                bake, archery, bike, run, play video games, and going to concerts.  
+            </p>
+           
+        </article>
+    );
+};
+
+export default About;
