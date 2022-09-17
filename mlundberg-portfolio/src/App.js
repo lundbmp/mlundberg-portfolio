dfe92236b891762bd4c54d7098057fc5f21528ea
lundbmp <m.lundberg008@gmail.com>
2022-09-17 13:30:32 -0500

    header and some aboutme stuff

--- mlundberg-portfolio/src/App.js
+++ mlundberg-portfolio/src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import About from "./components/About";
 import Projects from "./components/Projects";
 
 function App() {
+  const [aboutSelected, setAboutSelected] = useState(true);
+
   return (
     <div>
-      <About></About>
+      <Header
+        setAboutSelected={setAboutSelected}
+      ></Header>
+      <main>{aboutSelected ? <About></About> : <Projects></Projects>}</main>
     </div>
   );
 }
--- mlundberg-portfolio/src/components/Header/index.js
+++ mlundberg-portfolio/src/components/Header/index.js
@@ -1,11 +1,63 @@
 import React from "react";
 
-function Header() {
-    return (
-        <header>
-            
-        </header>
-    );
+function Header({ setAboutSelected }) {
+  return (
+    <header className="container-fluid">
+      <div className="row">
+        <div>
+          <h2 className="col">
+            <a data-testid="link" href="/">
+              Michael Lundberg
+            </a>
+          </h2>
+        </div>
+        <nav className="row justify-content-left">
+          <a
+            href="#about"
+            className="col-1"
+            onClick={() => setAboutSelected(true)}
+          >
+            About me
+          </a>
+          <a
+            href="#projects"
+            className="col-1"
+            onClick={() => setAboutSelected(false)}
+          >
+            {" "}
+            Projects
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.linkedin.com/in/michael-lundberg-b64964151/"
+            className="col-1"
+          >
+            {" "}
+            Linkedin
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/lundbmp"
+            className="col-1"
+          >
+            {" "}
+            GitHub
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="mailto:m.lundberg008@gmail.com"
+            className="col-1"
+          >
+            {" "}
+            Email
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
